3f6854cba14646889960cc6a16aff535f656aafb
Florestan Bredow <florestan.bredow@supagro.fr>
2015-10-30 10:34:19 +0100

    mise a jour de la configuration par défaut

--- ferme.config.sample.php
+++ ferme.config.sample.php
@@ -1,14 +1,19 @@
 <?php
 $this->config = array(
-    'db_host' => "localhost",
-    'db_name' => "XXXXXXXXXX",
-    'db_user' => "XXXXXXXXXX",
-    'db_password' => "XXXXXXXXXX",
-    'base_url' => "http://localhost/ferme/",
-    'source' => "yeswiki-cercopitheque", //
-    'ferme_path' => "./wikis/",
-    'archives_path' => "./archives/",
+    'db_host' => 'localhost',
+    'db_name' => 'XXXXXXXXXX',
+    'db_user' => 'XXXXXXXXXX',
+    'db_password' => 'XXXXXXXXXX',
+    'base_url' => 'http://localhost/ferme/',
+    'source' => 'yeswiki-cercopitheque',
+    'ferme_path' => './wikis/',
+    'archives_path' => './archives/',
     'tmp_path' => '/tmp/',
-    'template' => "default", // Template used
-    'admin_password' => md5("password"), // Default md5 password for installed wikis
+    'template' => 'default',
+    'users' => array(
+        // Les mots de passe doivent être chiffré
+        'admin' => password_hash('password', PASSWORD_DEFAULT),
+    ),
+    // Default md5 password for installed wikis
+    'admin_password' => md5('password'),
 );
