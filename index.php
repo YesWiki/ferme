b8e82c3d742dcdf442581a8a2d30e6a5a4b76def
Florestan Bredow <daiko@daiko.fr>
2016-10-26 16:28:22 +0200

    WIP : test sales a ranger au bon endroit

--- index.php
+++ index.php
@@ -5,6 +5,28 @@ $loader = require __DIR__ . '/vendor/autoload.php';
 
 session_start();
 
+if (!file_exists('vendor')) {
+    throw new \Exception(
+        'Vous devez executer "composer install" dans le dossier de la Ferme.',
+        1
+    );
+}
+
+if (!file_exists('wikis')) {
+    mkdir('wikis', 0777, true);
+}
+
+if (!file_exists('archives')) {
+    mkdir('archives', 0777, true);
+}
+
+if (!file_exists('ferme.config.php')) {
+    throw new \Exception(
+        'Le fichier de configuration est absent.',
+        1
+    );
+}
+
 try {
     $config = new Configuration('ferme.config.php');
     $ferme = new Ferme($config);
